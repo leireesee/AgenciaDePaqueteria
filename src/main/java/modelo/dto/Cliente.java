a5803f00ac572f2f2d12b509b8acd317e4d1ad11
nagore <znagoresanchez@gmail.com>
2023-03-27 13:34:33 +0200

    atributos

---
+++ src/main/java/modelo/dto/Cliente.java
@@ -0,0 +1,12 @@
+package modelo.dto;
+
+public abstract class Cliente  {
+
+	private String nombre;
+	private String direccion;
+	private String formaPago;
+	private String telefono;
+	private String contrasena;
+	
+	
+}
