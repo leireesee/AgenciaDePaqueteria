d6b37281f6c0271590373a4fc6a2b79e33cb7e76
nagore <znagoresanchez@gmail.com>
2023-03-27 13:54:06 +0200

    atributos, setters and gettes

---
+++ src/main/java/modelo/dto/Particular.java
@@ -0,0 +1,15 @@
+package modelo.dto;
+
+public class Particular extends Cliente {
+	private String dni;
+	// setter and getters
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+}
