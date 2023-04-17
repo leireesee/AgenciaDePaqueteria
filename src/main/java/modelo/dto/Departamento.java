25d8bab2c3ab18a8fb55f6dc4b215c1c34c4ae87
nagore <znagoresanchez@gmail.com>
2023-04-17 09:50:53 +0200

    clase anadida y mejroa

---
+++ src/main/java/modelo/dto/Departamento.java
@@ -0,0 +1,23 @@
+package modelo.dto;
+
+public class Departamento {
+
+	private int codDepartamento;
+	private String nombre;
+	
+	//getters and setters
+	public int getCodDepartamento() {
+		return codDepartamento;
+	}
+	public void setCodDepartamento(int codDepartamento) {
+		this.codDepartamento = codDepartamento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
