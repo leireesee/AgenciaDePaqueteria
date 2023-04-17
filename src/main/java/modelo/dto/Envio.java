7bcc5495288ea52f26648b2a2c474b5e1559fd28
nagore <znagoresanchez@gmail.com>
2023-04-17 09:39:19 +0200

    cosas mejoradas

--- src/main/java/modelo/dto/Envio.java
+++ src/main/java/modelo/dto/Envio.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public abstract class Envio {
 
 	private int codEnvio; //autoincrement en la base de datos
+	private int codSucursal;
 	private String idCliente;
 	private Date fechaEntrada;
 	private Date fechaSalida;
@@ -13,74 +14,69 @@ public abstract class Envio {
 	private String direccionDestino;
 	private String tracking;
 	
-	private Sucursal sucursal;
 	
 	
-
 	// setter and getters
 
 	public int getCodEnvio() {
 		return codEnvio;
 	}
-
 	public void setCodEnvio(int codEnvio) {
 		this.codEnvio = codEnvio;
 	}
-
+	public int getCodSucursal() {
+		return codSucursal;
+	}
+	public void setCodSucursal(int codSucursal) {
+		this.codSucursal = codSucursal;
+	}
 	public String getIdCliente() {
 		return idCliente;
 	}
-
 	public void setIdCliente(String idCliente) {
 		this.idCliente = idCliente;
 	}
-
 	public Date getFechaEntrada() {
 		return fechaEntrada;
 	}
-
 	public void setFechaEntrada(Date fechaEntrada) {
 		this.fechaEntrada = fechaEntrada;
 	}
-
 	public Date getFechaSalida() {
 		return fechaSalida;
 	}
-
 	public void setFechaSalida(Date fechaSalida) {
 		this.fechaSalida = fechaSalida;
 	}
-
 	public Date getFechaLlegada() {
 		return fechaLlegada;
 	}
-
 	public void setFechaLlegada(Date fechaLlegada) {
 		this.fechaLlegada = fechaLlegada;
 	}
-
 	public boolean isEntregado() {
 		return entregado;
 	}
-
 	public void setEntregado(boolean entregado) {
 		this.entregado = entregado;
 	}
-
 	public String getDireccionDestino() {
 		return direccionDestino;
 	}
-
 	public void setDireccionDestino(String direccionDestino) {
 		this.direccionDestino = direccionDestino;
 	}
-
 	public String getTracking() {
 		return tracking;
 	}
-
 	public void setTracking(String tracking) {
 		this.tracking = tracking;
 	}
+	
+	
+	
+	
+
+
 
 }
