a396fc47eb2d550bbe35b1ed2aced910a44054f4
nagore <znagoresanchez@gmail.com>
2023-04-18 09:23:33 +0200

    metodo insertar

--- src/main/java/modelo/dao/ModeloEnvio.java
+++ src/main/java/modelo/dao/ModeloEnvio.java
@@ -1,36 +1,54 @@
 package modelo.dao;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import modelo.dto.Cliente;
 import modelo.dto.Envio;
 
-public class ModeloEnvio {
+public class ModeloEnvio extends Conector {
 
-	
-	public void insertarEnvio() {
-		
-	}
-	
+	public void insertarEnvio(Envio envio) {
+		PreparedStatement pstInsert;
+		try {
+			pstInsert = super.conexion.prepareStatement(
+					"INSERT INTO envio (cod_sucursal, id_cliente, fecha_entrada, fecha_salida, fecha_llegada, entregado, direccion_destino, tracking)");
+			pstInsert.setInt(1, envio.getCodSucursal());
+			pstInsert.setString(2, envio.getIdCliente());
+			pstInsert.setDate(3, new Date(envio.getFechaEntrada().getTime()));
+			pstInsert.setDate(4, new Date(envio.getFechaSalida().getTime()));
+			pstInsert.setDate(5, new Date(envio.getFechaLlegada().getTime()));
+			pstInsert.setBoolean(6, envio.isEntregado());
+			pstInsert.setString(7, envio.getDireccionDestino());
+			pstInsert.setString(8, envio.getTracking());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}// fin clase insertar
+
 	public void eliminarEnvio() {
-		
+
 	}
-	
+
 	public void modificarEnvio() {
-		
+
 	}
-	
-	public  ArrayList<Envio> verEnvios(){
-		
+
+	public ArrayList<Envio> verEnvios() {
+
 		return null;
 	}
-	
+
 	public Envio verEnvio() {
 		return null;
 	}
-	public String  verTracking() {
-		
+
+	public String verTracking() {
+
 		return null;
 	}
-	
+
 }
