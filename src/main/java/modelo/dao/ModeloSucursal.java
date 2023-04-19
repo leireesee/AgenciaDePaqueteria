2576d76f3edb7f35b20f3ab3e6b596bf99f5394b
nagore <znagoresanchez@gmail.com>
2023-04-19 09:29:39 +0200

    sucursal

--- src/main/java/modelo/dao/ModeloSucursal.java
+++ src/main/java/modelo/dao/ModeloSucursal.java
@@ -1,5 +1,37 @@
 package modelo.dao;
 
-public class ModeloSucursal {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-}
+import modelo.dto.Sucursal;
+
+public class ModeloSucursal extends Conector {
+
+	public void insertarSucursa(Sucursal sucursal) {
+		PreparedStatement pstInsert;
+
+		try {
+			pstInsert = super.conexion.prepareStatement("INSERT INTO sucursal (telefono, direccion) VALUES (?,?)");
+
+			pstInsert.setString(1, sucursal.getTelefono());
+			pstInsert.setString(2, sucursal.getDireccion());
+			pstInsert.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void eliminarSucursal (int cod_sucursal) {
+		PreparedStatement pstDelete;
+		try {
+			pstDelete = super.conexion.prepareStatement("DELETE FROM sucursal WHERE cod_sucursal=?");
+			pstDelete.setInt(1, cod_sucursal);
+			pstDelete.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}//fin clase
