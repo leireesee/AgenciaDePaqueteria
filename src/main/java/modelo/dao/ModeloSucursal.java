c0077758ceaab66e041731296e20f04c0f99716b
nagore <znagoresanchez@gmail.com>
2023-04-21 09:11:10 +0200

    poner comentarios

--- src/main/java/modelo/dao/ModeloSucursal.java
+++ src/main/java/modelo/dao/ModeloSucursal.java
@@ -10,32 +10,32 @@ import modelo.dto.Sucursal;
 
 public class ModeloSucursal extends Conector {
 
-	public void insertarSucursa(Sucursal sucursal) {
-		PreparedStatement pstInsert;
-
-		try {
-			pstInsert = super.conexion.prepareStatement("INSERT INTO sucursal (telefono, direccion) VALUES (?,?)");
-
-			pstInsert.setString(1, sucursal.getTelefono());
-			pstInsert.setString(2, sucursal.getDireccion());
-			pstInsert.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void eliminarSucursal(int cod_sucursal) {
-		PreparedStatement pstDelete;
-		try {
-			pstDelete = super.conexion.prepareStatement("DELETE FROM sucursal WHERE cod_sucursal=?");
-			pstDelete.setInt(1, cod_sucursal);
-			pstDelete.execute();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void insertarSucursa(Sucursal sucursal) {
+//		PreparedStatement pstInsert;
+//
+//		try {
+//			pstInsert = super.conexion.prepareStatement("INSERT INTO sucursal (telefono, direccion) VALUES (?,?)");
+//
+//			pstInsert.setString(1, sucursal.getTelefono());
+//			pstInsert.setString(2, sucursal.getDireccion());
+//			pstInsert.execute();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//	public void eliminarSucursal(int cod_sucursal) {
+//		PreparedStatement pstDelete;
+//		try {
+//			pstDelete = super.conexion.prepareStatement("DELETE FROM sucursal WHERE cod_sucursal=?");
+//			pstDelete.setInt(1, cod_sucursal);
+//			pstDelete.execute();
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	public Sucursal verSucursal(int codSucursal) {
 
